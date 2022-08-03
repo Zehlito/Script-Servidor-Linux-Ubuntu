31ddd32c8a365e3a47ff834fc0302f567c3037f5
Junior Studzinski <junior992752884@yahoo.com>
2022-08-03 09:12:19 -0300

    Apache vol.1

---
+++ script-apache2.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Atualizando o Servidor"
+
+apt-get update -y
+apt-get upgrade -y
+apt-get install apache2 unzip -y
+cd /tmp
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+unzip main.zip
+cd linux-site-dio-main
+cp -R * /var/www/html/
+
+
+echo "Fim da Operação! "
